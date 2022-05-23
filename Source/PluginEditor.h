b10c98ce61d7c08f4cb75e773412cdd0fbd5f21f
Squadquiz <amornsakstp6@gmail.com>
2022-05-24 01:04:52 +0700

    Started displaying parameter min/max values

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -45,6 +45,15 @@ struct RotarySliderWithLabels : juce::Slider
     {
         setLookAndFeel(nullptr);
     }
+
+    struct LabelPos
+    {
+        float pos;
+        juce::String label;
+    };
+
+    juce::Array<LabelPos> labels;
+
     //==============================================================================
 
     void paint(juce::Graphics& g) override;
